d3a18b8e30baa9298d36235a029d73242caf1c4a
Pranav Tharammal <1032230006@tcetmumbai.in>
2025-08-19 21:53:19 +0530

    Update Exp5.py

--- Exp5.py
+++ Exp5.py
@@ -1,7 +1,1 @@
-from flask import Flask
-Exp5 = Flask(__name__)
-@Exp5.route('/')
-def hello():
-    return "Hello from Jenkins!!!"
-if __name__ == '__main__':
-    Exp5.run(debug=True) 
+print("Hello from Jenkins!!!")
